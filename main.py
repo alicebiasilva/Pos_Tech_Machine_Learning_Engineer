958da699dbececbcd0a799e1555af1f93c545fbe
Alice Beatriz <alicebiasilva24@gmail.com>
2025-10-10 22:52:26 -0300

    Ajuste frontend 2.0

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tech_challenge.api.routes import router
 from tech_challenge.api.config import Config
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title=Config.SWAGGER_TITLE,
@@ -9,3 +10,6 @@ app = FastAPI(
 )
 
 app.include_router(router, prefix="/api/v1")
+
+# Monta a pasta 'frontend' como arquivos estáticos
+app.mount("/", StaticFiles(directory="tech_challenge/frontend", html=True), name="frontend")
